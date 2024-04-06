675797e805e915300c934216d3b4f4c2a41b8791
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-04-06 15:07:45 +0100

    render test

--- flask/app.py
+++ flask/app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/projects")
+def home():
+    return render_template("projects.html")
+
+@app.route("/skills")
+def home():
+    return render_template("skills.html")
+
+@app.route("/about")
+def home():
+    return render_template("about.html")
+
+@app.route("/contacts")
+def home():
+    return render_template("contacts.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
