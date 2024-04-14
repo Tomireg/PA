6e69b1145d06161b0622385effc04333eb4c3043
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-04-14 11:51:01 +0100

    final

--- flask/app.py
+++ flask/app.py
@@ -27,17 +27,5 @@ def contact():
 def home():
     return render_template("index.html")
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
-    else:
-        return render_template("login.html")
-
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
-
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
