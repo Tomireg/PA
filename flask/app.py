b21fd4516991035234688e2da61c120a56ca4a5b
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-06-07 19:25:30 +0100

    new stuff

--- flask/app.py
+++ flask/app.py
@@ -23,17 +23,9 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/game")
 def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
-    else:
-        return render_template("login.html")
-
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+        return render_template("game.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
